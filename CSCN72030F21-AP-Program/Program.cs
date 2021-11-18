3d251db06893f0b5c1580ff4619e779bf05f55bb
Anthony Sasso <71456585+anthonysasso2001@users.noreply.github.com>
2021-11-18 09:58:44 -0500

    added project to split up classes, main, tests

---
+++ CSCN72030F21-AP-Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSCN72030F21_AP_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSCN72030F21-AP-Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSCN72030F21_AP_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
