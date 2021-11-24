7c0921e20a897d93161e988861cb3b7319a24175
TraLe <Tle4486@conestogac.on.ca>
2021-11-24 17:51:48 -0500

    Add display() in Liquid Level module

--- CSCN72030F21-AP-Classes/LiquidLevel.cs
+++ CSCN72030F21-AP-Classes/LiquidLevel.cs
@@ -15,6 +15,24 @@ namespace CSCN72030F21_AP_Classes
 
         public override bool display(int inputTime)
         {
+            int liquidPercentage = Int32.Parse(fileGet(0));
+            Console.Write("The current liquid level is:");
+            if (liquidPercentage > 25)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(fileGet(0) + "\n");
+            }
+            else if (liquidPercentage == 25)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(fileGet(0) + "\n");
+                Console.WriteLine("Warning: Liquid Level is low");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(fileGet(0) + "\n");
+            }
             return true;
         }
         public override bool modify(string inputValue)
