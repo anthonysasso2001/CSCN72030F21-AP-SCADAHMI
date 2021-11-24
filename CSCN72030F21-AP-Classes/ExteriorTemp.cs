33ec028d95e489fa7de80791c6043cfee6f23007
TraLe <Tle4486@conestogac.on.ca>
2021-11-24 16:59:43 -0500

    Complete Exterior Temperature Module

--- CSCN72030F21-AP-Classes/ExteriorTemp.cs
+++ CSCN72030F21-AP-Classes/ExteriorTemp.cs
@@ -10,11 +10,28 @@ namespace CSCN72030F21_AP_Classes
     {
         public ExteriorTemp(string inputFileName) : base(inputFileName, false)
         {
-
+            
         }
-
+         
         public override bool display(int inputTime)
         {
+            int extTemp = Int32.Parse(fileGet(0));
+            Console.Write("The current exterior temperature is:");
+            if(extTemp < (-40))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(fileGet(0) + "\n");
+            }
+            else if(extTemp >50)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(fileGet(0) + "\n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(fileGet(0) + "\n");
+            }
             return true;
         }
         public override bool modify(string inputValue)
